7887918c2c1b0f31211fbff1a0f7be5441e59296
Ubuntu <ubuntu@ip-172-31-37-93.us-east-2.compute.internal>
2021-06-22 18:28:37 +0000

    some Dockerfiles for Arm tools

---
+++ corstone-300-ml-eval-kit/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --rm --privileged --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY $*  armswdev/arm-tools:corstone-300-ml-eval-kit
